989344e76c4c49089fcb3157107052bce35c36cd
Chase Covington <mchasecov@gmail.com>
2022-01-28 16:39:19 -0500

    refactor URL and host settings

--- lib/omniauth/strategies/bnet.rb
+++ lib/omniauth/strategies/bnet.rb
@@ -6,20 +6,15 @@ module OmniAuth
     class Bnet < OmniAuth::Strategies::OAuth2
       option :region, 'us'
       option :client_options, {
-        :scope => 'wow.profile sc2.profile'
+        scope: 'wow.profile sc2.profile'
       }
 
       def client
+        host = get_host(options.region)
         # Setup urls based on region option
-        if !options.client_options.has_key(:authorize_url)
-          options.client_options[:authorize_url] = "https://#{getHost(options.region)}/oauth/authorize"
-        end
-        if !options.client_options.has_key(:token_url)
-          options.client_options[:token_url] = "https://#{getHost(options.region)}/oauth/token"
-        end
-        if !options.client_options.has_key(:site)
-          options.client_options[:site] = "https://#{getHost(options.region)}/"
-        end
+        set_client_url(:authorize_url, "https://#{host}/oauth/authorize")
+        set_client_url(:token_url, "https://#{host}/oauth/token")
+        set_client_url(:site, "https://#{host}/")
 
         super
       end
@@ -31,9 +26,7 @@ module OmniAuth
       def authorize_params
         super.tap do |params|
           %w[scope client_options].each do |v|
-            if request.params[v]
-              params[v.to_sym] = request.params[v]
-            end
+            params[v.to_sym] = request.params[v] if request.params[v]
           end
         end
       end
@@ -54,17 +47,18 @@ module OmniAuth
 
       private
 
+      def set_client_url(key, default_url)
+        return if options.client.include?(key)
+
+        options.client_options[key] = default_url
+      end
+
       def callback_url
         full_host + script_name + callback_path
       end
 
-      def getHost(region)
-        case region
-        when "cn"
-          "www.battlenet.com.cn"
-        else
-          "#{region}.battle.net"
-        end
+      def get_host(region)
+        region == 'cn' ? 'www.battlenet.com.cn' : "#{region}.battle.net"
       end
     end
   end
